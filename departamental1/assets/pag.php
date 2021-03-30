5df6010bd00785c245daea90375b35b3ebcab24c
EfrainIbarra <54225051+EfrainIbarra@users.noreply.github.com>
2021-03-30 00:41:22 -0600

    Update pag.php

--- departamental1/assets/pag.php
+++ departamental1/assets/pag.php
@@ -1,4 +1,45 @@
 <?php
-    header("location: http://kirabelmont.byethost14.com/index.html");
-    die();
+    $name = $_POST['name'];
+    $ontas = $_POST['ontas'];
+    $anime = $_POST['anime'];
+    $color = $_POST['color'];
+    
+    switch($anime){
+        case 1:
+            $texto = "no";
+            break;
+        case 2:
+            $texto = "Shojo";
+            break;
+        case 3:
+            $texto = "Shonen";
+            break;
+        case 4:
+            $texto = "Gore";
+            break;
+        case 5:
+            $texto = "Ficcion";
+            break;
+        default:
+            $texto = "Nose";
+            break;
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles4.css"/>
+    <title>Hola</title>
+</head>
+<body>
+    <h1>Hola <?php echo $name;?></h1>
+    <fieldset>
+        <p>Ten mas cuidado con a quien le das tus datos personales ¿va? me cabas de decir que estas en "<?php echo $ontas; ?>". 
+         Pero bueno, tu genero de anime preferido es el <?php echo $texto;?> y elegiste el color <?php echo $color;?>, te perdono, por ello no enviare a nadie a por ti.</p>
+</fieldset>
+</body>
+</html>
