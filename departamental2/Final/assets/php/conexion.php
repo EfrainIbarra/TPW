38954684680f5756e2e303220e9e4bc873cea9f8
EfrainIbarra <54225051+EfrainIbarra@users.noreply.github.com>
2021-06-04 10:08:22 -0500

    Update conexion.php

--- departamental2/Final/assets/php/conexion.php
+++ departamental2/Final/assets/php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servidor = "sql309.mipropia.com";
-    $usuario = "mipc_28423953";
-    $clave = "C4nc3r";
-    $base = "mipc_28423953_electivas";
+    $servidor = "localhost";
+    $usuario = "root";
+    $clave = "";
+    $base = "electivas";
 
     $conexion = mysqli_connect($servidor,$usuario,$clave,$base);
     mysqli_set_charset($conexion,"utf8");
